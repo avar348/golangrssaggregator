e6c448f531934e27a792451693474f0b12fbe0a9
Alex Vargas <alexvargas561@gmail.com>
2024-09-07 23:20:10 -0700

    added the ability to create post related to the feeds

--- scraper.go
+++ scraper.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 
 	"github.com/avar348/golangrssaggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(db *database.Queries,
@@ -49,7 +51,32 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found Post", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error trying to parse date %v with error %v", item.PubDate, err)
+		}
+
+		_, err = db.CreatePost(context.Background(),
+			database.CreatePostParams{
+				ID:          uuid.New(),
+				CreatedAt:   time.Now().UTC(),
+				UpdatedAt:   time.Now().UTC(),
+				Title:       item.Title,
+				Description: description,
+				PublishedAt: pubAt,
+				Url:         item.Link,
+				FeedID:      feed.ID,
+			})
+
+		if err != nil {
+			log.Println("Failed to create post:", err)
+		}
 	}
 
 	log.Printf("Feed %s collected, %v post found", feed.Name, len(rssFeed.Channel.Item))
